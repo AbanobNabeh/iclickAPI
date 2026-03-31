189f4144474d093d17a836179d1bd09fb9a51be9
Abanob Nabeh <abanobnabehrady@gmail.com>
2026-03-31 16:47:55 +0200

    Verify OTP ID

--- database/migrations/2024_04_02_071834_o_t_p_verify.php
+++ database/migrations/2024_04_02_071834_o_t_p_verify.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('verifyotp', function (Blueprint $table) {
+                  $table->id();
             $table->string('email')->unique();;
             $table->string('otp');
         });
